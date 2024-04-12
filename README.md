6c02ee0c9a4e3ec79107382225e4e555bf5bd0aa
Deep Mitra <137201089+Jupiter-2003@users.noreply.github.com>
2024-04-12 21:21:07 +0530

    Completed the project for rps game

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-paper-scissors
+This is a basic Rock, Paper, Scissors Game.
+Project for The Odin Project.
+
+It consists of an HTML file and a JS file.
+The JS file contains the game code.
+
+Link:
+(jupiter-2003.github.io/rock-paper-scissors/)
---
+++ rps.js
@@ -0,0 +1,77 @@
+playGame();
+
+function getComputerChoice(){
+    switch(Math.floor(Math.random()*3)+1)
+    {
+        case 1: return "R";
+        break;
+        case 2: return "S";
+        break;
+        case 3: return "P";
+    }
+}
+
+function singleRound(computerSelection){
+    let playerSelectionWord = prompt("Enter rock/R, paper/P or scissors/S. Any other selection will result in loss");
+    let playerSelection = (playerSelectionWord.toUpperCase()).charAt(0);
+    if(computerSelection=='R'){
+        if(playerSelection=='R'){
+            alert("Draw");
+            return 0;
+        }
+        else if(playerSelection=='P'){
+            alert("You win this round!");
+            return 1;
+        }
+        else{
+            alert("You lose this round!");
+            return -1;
+        }
+    }
+    else if(computerSelection=='P'){
+        if(playerSelection=='P'){
+            alert("Draw");
+            return 0;
+        }
+        else if(playerSelection=='S'){
+            alert("You win this round!");
+            return 1;
+        }
+        else{
+            alert("You lose this round!");
+            return -1;
+        }
+    }
+    else if(computerSelection=='S'){
+        if(playerSelection=='S'){
+            alert("Draw");
+            return 0;
+        }
+        else if(playerSelection=='R'){
+            alert("You win this round!");
+            return 1;
+        }
+        else{
+            alert("You lose this round!");
+            return -1;
+        }
+    }
+
+}
+
+function playGame()
+{
+    let score = 0;
+    for(let i=1;i<=5;i++){
+        score+=singleRound(getComputerChoice());
+    }
+    if(score>0){
+        alert("You won this Game!!!");
+    }
+    else if(score<0){
+        alert("Computer won this Game!!!");
+    }
+    else{
+        alert("It's a draw!!!");
+    }
+}
