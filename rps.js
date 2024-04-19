48900e407e0c28414bd72a4b1806266ea0f9a3e7
Deep Mitra <137201089+Jupiter-2003@users.noreply.github.com>
2024-04-19 17:44:20 +0530

    updated js

--- rps.js
+++ rps.js
@@ -1,24 +1,45 @@
-playGame();
+
+let playerScore=0, compScore=0, ctr=0;
+const buttons = document.querySelectorAll("button");
+const playSc = document.querySelector("#comp");
+const compSc = document.querySelector("#user");
+
+while(playerScore<5 || compScore<5){
+    buttons.forEach((button) => {
+        button.addEventListener("click",() => {
+            ctr = singleRound(getComputerChoice(), button.id);
+        });
+    });
+    if(ctr==1){
+        playerScore++;
+    }
+    else if(ctr==-1){
+        compScore++;
+    }
+    playSc.textContent = playerScore;
+    compSc.textContent = compScore;
+}
+if(playerScore==5){
+    playSc.textContent = "WON the Game!";
+    compSc.textContent = "LOST!";
+}
+else{
+    compSc.textContent = "WON the Game!";
+    playSc.textContent = "LOST!";
+}
+
+
 
 function getComputerChoice(){
     switch(Math.floor(Math.random()*3)+1)
     {
         case 1: return "R";
-        break;
         case 2: return "S";
-        break;
         case 3: return "P";
     }
 }
 
-function singleRound(computerSelection){
-    let playerSelection = '';
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach((button) => {
-        button.addEventListener("click",() => {
-            playerSelection = button.id;
-        });
-    });
+function singleRound(computerSelection, playerSelection){
     if(computerSelection=='R'){
         if(playerSelection=='R'){
             return 0;
@@ -53,30 +74,4 @@ function singleRound(computerSelection){
         }
     }
 
-}
-
-function playGame()
-{
-    let playerScore=0, compScore=0, ctr=0;
-    const playSc = document.querySelector("#comp");
-    const compSc = document.querySelector("#user");
-    while(playerScore<5 || compScore<5){
-        ctr = singleRound(getComputerChoice());
-        if(ctr==1){
-            playerScore++;
-        }
-        else if(ctr==-1){
-            compScore++;
-        }
-        playSc.textContent = playerScore;
-        compSc.textContent = compScore;
-    }
-    if(playerScore==5){
-        playSc.textContent = "WON the Game!";
-        compSc.textContent = "LOST!";
-    }
-    else{
-        compSc.textContent = "WON the Game!";
-        playSc.textContent = "LOST!";
-    }
 }
